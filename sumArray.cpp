e375a506531f129dc900aa9e0728a929913a98b4
prodhanm <mustafizur.prodhan@gmail.com>
2025-05-08 03:35:47 -0400

    The array gets the sum of all of its values

--- sumArray.cpp
+++ sumArray.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+using namespace std;
 
-int sumOfArray() {
-    return 0;
+int sumOfArray(int arr[], int size) {
+  int total = 0;
+  for (int i = 0; i < size; i++) {
+      total += arr[i];
+  }
+  return total;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+   int total;
+   int size;
+   cout << "Enter the size of the array: ";
+   cin >> size;
 
+   int* arr = new int[size];
+   cout << "Enter the elements of the array: ";
+   for (int i = 0; i < size; i++) {
+       cin >> arr[i];
+   }
+
+   total = sumOfArray(arr, size);
+   printf("The sum of the array is: %d", total);
+   return 0;
+}
